4594052636c5ed0bc56d09661cba5d838847f72c
Bui Quang Minh <minhcly95@outlook.com>
2024-02-14 12:49:12 -0500

    test: add benchmarks for SPerm

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,6 +2,12 @@ using FastPerms
 using BenchmarkTools
 
 SUITE = BenchmarkGroup()
-SUITE["rand"] = @benchmarkable rand(10)
 
-# Write your benchmarks here.
+SUITE["SPerm"] = BenchmarkGroup()
+for N in [4, 8, 16, 32, 64, 128], T in [Int, UInt8]
+    S = SPerm{N,T}
+    SUITE["SPerm"]["$S"] = BenchmarkGroup()
+    SUITE["SPerm"]["$S"]["mul"] = @benchmarkable a * b setup=(a=rand($S);b=rand($S))
+    SUITE["SPerm"]["$S"]["inv"] = @benchmarkable inv(a) setup=(a=rand($S))
+end
+
